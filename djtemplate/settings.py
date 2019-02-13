623d5b0ce1704353a8ba06be8e772618f4731fb3
Shogo Sawai <shogo.sawai@gmail.com>
2019-02-13 11:34:13 +0900

    update settings

--- djtemplate/settings.py
+++ djtemplate/settings.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """
 Django settings for djtemplate project.
 
@@ -10,11 +12,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import sys
 import os
+import time
+import datetime
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(__file__)
+PROJECT_NAME = os.path.basename(PROJECT_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -38,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+SESSION_COOKIE_NAME = '{}-sessionid'.format(PROJECT_NAME)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,13 +56,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+if DEBUG:
+    MIDDLEWARE.insert(1, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
-ROOT_URLCONF = 'djtemplate.urls'
+ROOT_URLCONF = '{}.urls'.format(PROJECT_NAME)
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,9 +77,67 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'djtemplate.wsgi.application'
+
+############################################
+# log
+############################################
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+            'datefmt': '%y %b %d, %H:%M:%S',
+        },
+        'verbose': {
+            'format': ('%(levelname)s %(asctime)s %(module)s '
+                       '%(process)d %(thread)d %(message)s'),
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'django.log',
+            'formatter': 'simple',
+            'maxBytes': 1024 * 1024 * 100,  # 100 mb
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        },
+    },
+    'loggers': {
+        os.path.basename(PROJECT_NAME): {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+############################################
+# wsgi
+############################################
+WSGI_APPLICATION = '{}.wsgi.application'.format(PROJECT_NAME)
 
 
+############################################
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -100,21 +168,84 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+############################################
 # Internationalization
+############################################
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
+# make timeaware datetime
+# t = datetime.datetime.now().replace(tzinfo=JST())
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'Asia/Tokyo'
+if sys.version_info[0] == 2:
+    class JST(datetime.tzinfo):
+        def utcoffset(self, dt):
+            return datetime.timedelta(hours=9)
 
-LANGUAGE_CODE = 'en-us'
+        def tzname(self, dt):
+            return "Asia/Tokyo"
 
-TIME_ZONE = 'UTC'
+        def dst(self, dt):
+            return datetime.timedelta(0)
+    TZ_INFO = JST()
+else:
+    TZ_INFO = datetime.timezone(datetime.timedelta(hours=9))
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
+############################################
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+############################################
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.environ.get(
+    'STATIC_ROOT', os.path.join(BASE_DIR, PROJECT_DIR, "static"))
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, PROJECT_DIR, "static"),
+)
+
+
+############################################
+# debug-toolbar
+############################################
+if DEBUG:
+    INSTALLED_APPS += [
+        'debug_toolbar',
+        'template_timings_panel'
+    ]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TEMPLATE_CONTEXT': True,
+    "SHOW_TOOLBAR_CALLBACK": lambda request: True,
+}
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'template_timings_panel.panels.TemplateTimings.TemplateTimings',
+]
+
+
+############################################
+# current project
+############################################
+MIDDLEWARE += [
+    'django_busybody.middlewares.GlobalRequestMiddleware',
+]
+
+INSTALLED_APPS = [
+] + INSTALLED_APPS
+
+
