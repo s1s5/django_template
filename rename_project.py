0b0f0960983f309349b260080bad711d794a718e
Shogo Sawai <shogo.sawai@gmail.com>
2019-02-13 12:12:18 +0900

    implement rename_project

--- rename_project.py
+++ rename_project.py
@@ -1,10 +1,36 @@
-# coding: utf-8
+#!/usr/bin/env python
+import os
+import itertools
 import glob
+import shutil
 
-def main(args):
+def replace_file(filename, from_string, to_string):
+    contents = open(filename, 'r').read()
+    contents = contents.replace(from_string, to_string)
+    open(filename, 'w').write(contents)
+
+
+def main(new_project_name, port):
+    ignore = [
+        'rename_project.py'
+        'readme.md',
+        'LICENSE',
+        '__pycache__',
+    ]
     
-    pass
+    replace_file('docker-compose.override.yml', '42040:8000', '{}:8000'.format(port))
+
+    for filename in itertools.chain(glob.glob('*'), glob.glob('**/*')):
+        if os.path.isdir(filename):
+            continue
+        for j in ignore:
+            if j in filename:
+                continue
+        replace_file(filename, 'djtemplate', new_project_name)
+
+    shutil.move('djtemplate', new_project_name)
+
 
 if __name__ == '__main__':
     import sys
-    main(sys.argv[1:])
+    main(*sys.argv[1:])
